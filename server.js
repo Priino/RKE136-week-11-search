fc69b9060ea933b92ed2b7c2a7fd148c6a5abd56
Priit P. <priit.printsmann@tktk.ee>
2024-03-23 14:36:52 +0200

    .2

--- server.js
+++ server.js
@@ -51,6 +51,6 @@ app.post('/search', (req, res) => {
     }));
 });
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('server is running');
 });
